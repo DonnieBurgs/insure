8827a69807c69517343bda9d54f243aa950bd983
kidd3166 <hellokiki3166@163.com>
2017-04-07 17:23:12 +0800

    123

---
+++ src/com/insure/servlet/admin/EmInsurerCompanyServlet.java
@@ -0,0 +1,249 @@
+package com.insure.servlet.admin;
+/*
+	<servlet>
+		<servlet-name>emInsurerCompany</servlet-name>
+		<servlet-class>com.web.servlet.admin.EmInsurerCompanyServlet</servlet-class>
+	</servlet>
+	<servlet-mapping>
+		<servlet-name>emInsurerCompany</servlet-name>
+		<url-pattern>/emInsurerCompany.do</url-pattern>
+	</servlet-mapping>
+
+ */
+import java.io.*;
+import java.sql.*;
+import java.util.*;
+import java.util.Date;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.web.db.DbUtils;
+import com.web.util.*;
+
+public class EmInsurerCompanyServlet extends UserSecureDispatcher {
+
+	private static final long serialVersionUID = 1491555808333L;
+	
+	public void blank(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+    	if(checkAuthority("auth14", request, response)) {
+    		return;
+    	}
+		String uf_parentid = Putil.getString(request.getParameter("uf_parentid")) ;
+		request.setAttribute("uf_parentid", uf_parentid);
+
+		String keyword = Putil.getString(request.getParameter("keyword")) ;
+		int s = Putil.getInt(request.getParameter("s")); 
+		int m = Putil.getInt(request.getParameter("m")); 
+		request.setAttribute("keyword", keyword);
+		request.setAttribute("m", m+"");
+		request.setAttribute("s", s+"");
+
+		forward(request, response, "/admin/InsurerCompanyAdd.jsp");
+	
+	}
+
+	public void add(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		if(checkAuthority("auth14", request, response)) {
+    		return;
+    	}
+		String uf_parentid = Putil.getString(request.getParameter("uf_parentid")) ;
+		request.setAttribute("uf_parentid", uf_parentid);
+
+		String keyword = Putil.getString(request.getParameter("keyword")) ;
+		int s = Putil.getInt(request.getParameter("s")); 
+		int m = Putil.getInt(request.getParameter("m")); 
+		request.setAttribute("keyword", keyword);
+		request.setAttribute("m", m+"");
+		request.setAttribute("s", s+"");
+
+		Map<String, Object> admin = LoginManager.getUser(request);
+		if(admin==null) {
+			return ;
+		}
+		try {
+			String insurercompanyname = Putil.getString(request.getParameter("insurercompanyname"));
+			String insurercompanycode = Putil.getString(request.getParameter("insurercompanycode"));
+
+
+
+
+
+
+			
+			StringBuilder select = new StringBuilder("insert into em_insurercompany (insurercompanyname,insurercompanycode) values ("
+				+ "'" + insurercompanyname.replace("'", "''") + "'"
+				+ ",'" + insurercompanycode.replace("'", "''") + "'"
+				+ ")"
+			);
+
+			int result = DbUtils.save(select.toString());
+	
+		} catch (Exception e) {
+			e.printStackTrace(System.out);
+		}
+
+		redirect(request, response, "/emInsurerCompany.do?method=list&uf_parentid="+uf_parentid+"&keyword="+keyword+"&m="+m+"&s="+s);
+		
+	}
+
+	public void fill(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		String uf_parentid = Putil.getString(request.getParameter("uf_parentid")) ;
+		request.setAttribute("uf_parentid", uf_parentid);
+
+		String keyword = Putil.getString(request.getParameter("keyword")) ;
+		int s = Putil.getInt(request.getParameter("s")); 
+		int m = Putil.getInt(request.getParameter("m")); 
+		request.setAttribute("keyword", keyword);
+		request.setAttribute("m", m+"");
+		request.setAttribute("s", s+"");
+
+		String insurercompanyid = Putil.getString(request.getParameter("insurercompanyid")) ;
+		if(insurercompanyid.length()>0) {
+			Map<String, Object> row = DbUtils.queryOne("select p.* from em_insurercompany p where p.insurercompanyid="+insurercompanyid);
+			request.setAttribute("item", row);
+		}
+
+		forward(request, response, "/admin/InsurerCompanyEdit.jsp");
+		
+	}
+
+	public void update(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		if(checkAuthority("auth14", request, response)) {
+    		return;
+    	}
+		String uf_parentid = Putil.getString(request.getParameter("uf_parentid")) ;
+		request.setAttribute("uf_parentid", uf_parentid);
+
+		String keyword = Putil.getString(request.getParameter("keyword")) ;
+		int s = Putil.getInt(request.getParameter("s")); 
+		int m = Putil.getInt(request.getParameter("m")); 
+		request.setAttribute("keyword", keyword);
+		request.setAttribute("m", m+"");
+		request.setAttribute("s", s+"");
+
+		try {
+			String insurercompanyid = Putil.getString(request.getParameter("insurercompanyid")) ;
+			String insurercompanyname = Putil.getString(request.getParameter("insurercompanyname"));
+			String insurercompanycode = Putil.getString(request.getParameter("insurercompanycode"));
+
+
+
+
+			
+			StringBuilder select = new StringBuilder("update em_insurercompany set "
+					+ "insurercompanyname='" + insurercompanyname.replace("'", "''") + "'"
+					+ ",insurercompanycode='" + insurercompanycode.replace("'", "''") + "'"
+				+ " where insurercompanyid=" + insurercompanyid + "" 
+			);
+			int result = DbUtils.save(select.toString());
+
+		} catch (Exception e) {
+			e.printStackTrace(System.out);
+		} finally {
+		}
+
+		redirect(request, response, "/emInsurerCompany.do?method=list&uf_parentid="+uf_parentid+"&keyword="+keyword+"&m="+m+"&s="+s);
+		
+	}
+
+	public void list(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		String uf_parentid = Putil.getString(request.getParameter("uf_parentid")) ;
+		request.setAttribute("uf_parentid", uf_parentid);
+
+		try {
+			long totalCount = 0 ;
+			String keyword = Putil.getString(request.getParameter("keyword")) ;
+			int s = Putil.getInt(request.getParameter("s")); 
+			int m = Putil.getInt(request.getParameter("m")); 
+			String o = Putil.getString(request.getParameter("o")); // 排序字
+			String sort = Putil.getString(request.getParameter("sort")); // 顺序
+			
+
+			// 列表
+			if(s==0) s = 1 ;  //pagenum
+			if(m==0) m = 15 ;
+			List<Map<String, Object>> resultRows = new ArrayList<Map<String, Object>>();
+
+			StringBuilder countSql = new StringBuilder("select count(p.insurercompanyid) as total from em_insurercompany p where p.insurercompanyid>=0"
+					+ (keyword.length()>0?" and p.insurercompanyname like '%" + keyword + "%'":"")
+					);
+			Map<String, Object> countMap = DbUtils.queryOne(countSql.toString());
+			if (countMap!=null) {
+				totalCount = Putil.getInt(countMap.get("total"));
+			}
+			
+			// 列表分页语句
+			resultRows = DbUtils.query("select p.* from em_insurercompany p where p.insurercompanyid>=0"
+					+ (keyword.length()>0?" and p.insurercompanyname like '%" + keyword + "%'":"")
+					+ " order by p.seq desc limit " + (s-1)*m + "," + m + ""
+				);
+			
+			
+			request.setAttribute("totalRow", totalCount);
+			request.setAttribute("resultRows", resultRows);
+			request.setAttribute("keyword", keyword);
+			request.setAttribute("m", m+"");
+			request.setAttribute("s", s+"");
+
+		} catch (Exception e) {
+			e.printStackTrace(System.out);
+		} finally {
+		}
+
+		forward(request, response, "/admin/InsurerCompanyList.jsp");
+		
+	}
+
+	public void delete(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		if(checkAuthority("auth14", request, response)) {
+    		return;
+    	}
+		String uf_parentid = Putil.getString(request.getParameter("uf_parentid")) ;
+		request.setAttribute("uf_parentid", uf_parentid);
+
+		String keyword = Putil.getString(request.getParameter("keyword")) ;
+		int s = Putil.getInt(request.getParameter("s")); 
+		int m = Putil.getInt(request.getParameter("m")); 
+		request.setAttribute("keyword", keyword);
+		request.setAttribute("m", m+"");
+		request.setAttribute("s", s+"");
+
+		try {
+			String insurercompanyid = Putil.getString(request.getParameter("insurercompanyid"));
+
+			StringBuilder select = new StringBuilder("delete from em_insurercompany "
+					+ " where insurercompanyid=" + insurercompanyid + "" 
+					);
+
+			int result = DbUtils.save(select.toString());
+
+		} catch (Exception e) {
+			e.printStackTrace(System.out);
+		}
+
+		redirect(request, response, "/emInsurerCompany.do?method=list&uf_parentid="+uf_parentid+"&keyword="+keyword+"&m="+m+"&s="+s);
+		
+	}
+	
+    public boolean checkAuthority(HttpServletRequest request, HttpServletResponse response) {
+    	String auth13 = checkAuthority(13, request)!=null?"1":"";
+    	String auth14 = checkAuthority(14, request)!=null?"1":"";
+		request.setAttribute("auth13", auth13);
+		request.setAttribute("auth14", auth14);
+    	if(auth13.equals("") && auth14.equals("")) {
+    		this.prompt(response, "没有权限，请联系管理员！");
+    		return false;
+    	}
+    	return true;
+    	
+    }
+}
+
